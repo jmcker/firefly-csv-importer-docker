f54b388b6d4eaec158f8c87f7cec544707402f55
James Cole <thegrumpydictator@gmail.com>
2020-02-23 11:46:31 +0100

    Update inline script.

--- run-inline.sh
+++ run-inline.sh
@@ -16,10 +16,13 @@ FIREFLY_III_URI=http:/172.17.0.3/
 #
 # There is no need to touch anything after this point, but if you're smart you're free to do so.
 #
+DIR=$PWD
 
+
+echo "Link $DIR to /import"
 docker run \
 --rm \
--v $PWD:/import \
+-v $DIR:/import \
 -e FIREFLY_III_ACCESS_TOKEN=$PERSONAL_ACCESS_TOKEN \
 -e FIREFLY_III_URI=$FIREFLY_III_URI \
 -e WEB_SERVER=false \
