e1e183f0520289d4cede20970194ce2dc05c70c3
James Cole <thegrumpydictator@gmail.com>
2020-04-05 09:42:30 +0200

    Travis v2.0

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,23 +1,32 @@
 #!/usr/bin/env bash
 
-echo "travis.sh: I am building channel ${CHANNEL} for version ${VERSION} on architecture ${ARCH}, branch $RELEASE."
+#
+# Step 1: set repos name.
+#
+REPOS_NAME=fireflyiii/csv-importer
 
+#
+# Step 2: echo some info
+#
+echo "travis.sh v2.0: I am building '${VERSION}' on architecture ${ARCH} for ${REPOS_NAME}."
+
+#
+# Step 3: configure docker
+#
 echo '{"experimental":true}' | sudo tee /etc/docker/daemon.json
 mkdir $HOME/.docker
 touch $HOME/.docker/config.json
 echo '{"experimental":"enabled"}' | sudo tee $HOME/.docker/config.json
 sudo service docker restart
 
-
-# easy switch between test and pr repository.
-#REPOS_NAME=jc5x/ff-test-builds # jc5x/firefly-iii
-REPOS_NAME=fireflyiii/csv-importer # jc5x/firefly-iii
-
-
-
-# First build amd64 image:
+#
+# Step 4: login to docker
+#
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
+#
+# Step 5: optional ARM package.
+#
 if [ $ARCH == "arm" ]; then
     echo "Because architecture is $ARCH running some extra commands."
     docker run --rm --privileged multiarch/qemu-user-static:register --reset
@@ -30,81 +39,72 @@ if [ $ARCH == "arm" ]; then
     popd
 fi
 
-# if the release is develop, build and push develop. Don't push a version tag anymore.
-if [ $RELEASE == "develop" ]; then
-    LABEL=$REPOS_NAME:develop-$ARCH
-    echo "GitHub branch is $RELEASE. Will build and push $LABEL"
-    docker build -t $LABEL --build-arg RELEASE=$RELEASE -f Dockerfile.$ARCH .
-    docker push $LABEL
+#
+# Step 6: If version is "develop" build and push develop.
+# 
+if [[ $VERSION == "develop" ]]; then
+    BUILDLABEL=$REPOS_NAME:develop-$ARCH
+    echo "VERSION is $VERSION. Will build and push $BUILDLABEL."
+    docker build -t $BUILDLABEL --build-arg version=${VERSION} -f Dockerfile.$ARCH .
+    docker push $BUILDLABEL
+else
+    echo "'$VERSION' is not 'develop', skip this step."
 fi
 
-# if branch = master AND channel = alpha, build and push 'alpha'
-if [ $RELEASE == "master" ] && [ $CHANNEL == "alpha" ]; then
-    LABEL=$REPOS_NAME:alpha-$ARCH
-    echo "GitHub branch is $RELEASE and channel is $CHANNEL. Will build and push $LABEL"
-    docker build -t $LABEL --build-arg RELEASE=$RELEASE -f Dockerfile.$ARCH .
-    docker push $LABEL
+#
+# Step 7: If version like "alpha", build and push alpha.
+#
+if [[ $VERSION == *"alpha"* ]]; then
+    BUILDLABEL=$REPOS_NAME:alpha-$ARCH
+    echo "Version is alpha version '$VERSION'. Will build and push '$BUILDLABEL'."
+    docker build -t $BUILDLABEL --build-arg version=${VERSION} -f Dockerfile.$ARCH .
+    docker push $BUILDLABEL
+else
+    echo "'$VERSION' is NOT an alpha build. Step will be skipped."
 fi
 
-# if branch is master and channel is alpha, build and push 'alpha' and 'beta'.
-if [ $RELEASE == "master" ] && [ $CHANNEL == "beta" ]; then
-    LABEL=$REPOS_NAME:beta-$ARCH
-    echo "GitHub branch is $RELEASE and channel is $CHANNEL. Will build and push $LABEL"
-    docker build -t $LABEL --build-arg RELEASE=$RELEASE -f Dockerfile.$ARCH .
-    docker push $LABEL
-
-    # then tag as alpha and push:
-    docker tag $LABEL $REPOS_NAME:alpha-$ARCH
-    docker push $REPOS_NAME:alpha-$ARCH
-    echo "Also tagged $LABEL as $REPOS_NAME:alpha-$ARCH and pushed"
+#
+# Step 8: If version like "beta", build and push beta.
+#
+if [[ $VERSION == *"beta"* ]]; then
+    BUILDLABEL=$REPOS_NAME:beta-$ARCH
+    echo "Version is beta version '$VERSION'. Will build and push '$BUILDLABEL'."
+    docker build -t $BUILDLABEL --build-arg version=${VERSION} -f Dockerfile.$ARCH .
+    docker push $BUILDLABEL
+else
+    echo "'$VERSION' is NOT a beta build. Step will be skipped."
 fi
 
-# if branch is master and channel is stable, push 'alpha' and 'beta' and 'stable'.
-if [ $RELEASE == "master" ] && [ $CHANNEL == "stable" ]; then
+#
+# Step 9: If version not like alpha, beta, develop, build and push stable+latest
+#
+if [[ $VERSION != *"beta"* && $VERSION != *"alpha"* && $VERSION != *"develop"* ]]; then
+    echo "'$VERSION' is not beta or alpha. Build it."
+
     # first build stable
-    LABEL=$REPOS_NAME:stable-$ARCH
-    echo "GitHub branch is $RELEASE and channel is $CHANNEL. Will build and push $LABEL"
-    docker build -t $LABEL --build-arg RELEASE=$RELEASE -f Dockerfile.$ARCH .
-    docker push $LABEL
-
-    # then tag as beta and push:
-    docker tag $LABEL $REPOS_NAME:beta-$ARCH
-    docker push $REPOS_NAME:beta-$ARCH
-    echo "Also tagged $LABEL as $REPOS_NAME:beta-$ARCH and pushed"
-
-    # then tag as alpha and push:
-    docker tag $LABEL $REPOS_NAME:alpha-$ARCH
-    docker push $REPOS_NAME:alpha-$ARCH
-    echo "Also tagged $LABEL as $REPOS_NAME:alpha-$ARCH and pushed"
+    BUILDLABEL=$REPOS_NAME:stable-$ARCH
+    echo "VERSION is '$VERSION'. Will build and push $BUILDLABEL"
+    docker build -t $BUILDLABEL --build-arg version=${VERSION} -f Dockerfile.$ARCH .
+    docker push $BUILDLABEL
 
     # then tag as latest and push:
-    docker tag $LABEL $REPOS_NAME:latest-$ARCH
+    docker tag $BUILDLABEL $REPOS_NAME:latest-$ARCH
     docker push $REPOS_NAME:latest-$ARCH
-    echo "Also tagged $LABEL as $REPOS_NAME:latest-$ARCH and pushed"
+    echo "Also tagged $BUILDLABEL as $REPOS_NAME:latest-$ARCH and pushed"
+else
+    echo "'$VERSION' is NOT a stable build. Step will be skipped."
 fi
 
-# push to channel 'version' if master + alpha
-if [ $RELEASE == "master" ] && [ $CHANNEL == "alpha" ]; then
-    LABEL=$REPOS_NAME:version-$VERSION-$ARCH
-    echo "GitHub release is $RELEASE and channel is $CHANNEL. Will also push alpha as $LABEL"
-    docker tag $REPOS_NAME:alpha-$ARCH $LABEL
-    docker push $LABEL
-fi
-
-# push to channel 'version' if master + beta
-if [ $RELEASE == "master" ] && [ $CHANNEL == "beta" ]; then
-    LABEL=$REPOS_NAME:version-$VERSION-$ARCH
-    echo "GitHub release is $RELEASE and channel is $CHANNEL. Will also push beta as $LABEL"
-    docker tag $REPOS_NAME:beta-$ARCH $LABEL
-    docker push $LABEL
-fi
-
-# push to channel 'version' if master + stable
-if [ $RELEASE == "master" ] && [ $CHANNEL == "stable" ]; then
-    LABEL=$REPOS_NAME:version-$VERSION-$ARCH
-    echo "GitHub release is $RELEASE and channel is $CHANNEL. Will also push beta as $LABEL"
-    docker tag $REPOS_NAME:stable-$ARCH $LABEL
-    docker push $LABEL
+#
+# Step 10: If version not like "develop", build and push "version"
+#
+if [[ $VERSION == "develop" ]]; then
+    VERSIONLABEL=$REPOS_NAME:release-$VERSION-$ARCH
+    echo "Version is '$VERSION'. Will also push label '$VERSIONLABEL'"
+    docker tag $BUILDLABEL $VERSIONLABEL
+    docker push $VERSIONLABEL
+else
+    echo "'$VERSION' is 'develop', so will not push version."
 fi
 
 echo "Done!"
