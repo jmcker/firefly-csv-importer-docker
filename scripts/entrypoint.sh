5e942c5184f7cdd585c5f6abc349e6adbc47ea08
Jack McKernan <jmcker@outlook.com>
2020-07-12 22:46:58 -0500

    Add support for loading the access token from a _FILE variable
    Copied from https://github.com/firefly-iii/docker/blob/a9b2b1035a4592f25a927342e74cfbd0e411651d/scripts/entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,6 +3,40 @@
 echo "Now in entrypoint.sh (v1.2) for Firefly III CSV importer."
 echo "Please wait for the container to start..."
 
+# https://github.com/docker-library/wordpress/blob/master/docker-entrypoint.sh
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+# envs that can be appended with _FILE
+envs=(
+	FIREFLY_III_ACCESS_TOKEN
+)
+
+echo "Now parsing _FILE variables."
+for e in "${envs[@]}"; do
+  file_env "$e"
+done
+echo "done!"
+
 # make sure the correct directories exists (suggested by @chrif):
 mkdir -p $HOMEPATH/storage/app/public
 mkdir -p $HOMEPATH/storage/build
